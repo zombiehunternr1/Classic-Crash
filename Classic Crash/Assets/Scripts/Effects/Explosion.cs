c2d0ca8cc1127c457a211543025bcdcaf047a412
zombiehunternr1 <44666850+zombiehunternr1@users.noreply.github.com>
2021-04-19 16:21:51 +0200

    Collision detection improved
    - Removed OntriggerEnter detection from the particle effect since this method of checking wasn't the correct approach.
    - Implemented a function ghat checks the explosion radius with a Physics overlapphere and everything within that sphere gets check wether it's a player or crate.

--- Classic Crash/Assets/Scripts/Effects/Explosion.cs
+++ Classic Crash/Assets/Scripts/Effects/Explosion.cs
@@ -5,18 +5,30 @@ using UnityEngine;
 public class Explosion : MonoBehaviour
 {
     private SphereCollider HitBox;
+    public float ExplosionRadius = 1.5f;
 
     private void Awake()
     {
         HitBox = GetComponent<SphereCollider>();
+        CheckExplosionRadius();
         StartCoroutine(DisableCollider());
     }
 
-    public void OnTriggerEnter(Collider other)
+    void CheckExplosionRadius()
     {
-        if (other.GetComponent<InputManager>())
+        Collider[] hitColliders = Physics.OverlapSphere(HitBox.bounds.center, ExplosionRadius);
+        foreach (var hitCollider in hitColliders)
         {
-            Debug.Log("Hit player");
+            //Get reference to the script / interface
+            ICrateBase crate = (ICrateBase)hitCollider.gameObject.GetComponent(typeof(ICrateBase));
+            if (crate != null)
+            {
+                crate.Break(7);
+            }
+            else if(hitCollider.gameObject.GetComponent<InputManager>())
+            {
+                Debug.Log("Hit player");
+            }
         }
     }
 
