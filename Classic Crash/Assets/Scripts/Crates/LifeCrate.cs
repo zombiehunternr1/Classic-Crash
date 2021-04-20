e23abdc4ca07fda63d34cb7b0966566e727ddfa0
zombiehunternr1 <44666850+zombiehunternr1@users.noreply.github.com>
2021-04-20 15:48:51 +0200

    Implemented life crate
    Implemented life crate that drops a life at the crates position.
    - Added in a bool to check if it should auto add the amount to the player or not

---
+++ Classic Crash/Assets/Scripts/Crates/LifeCrate.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifeCrate : MonoBehaviour, ICrateBase
+{
+    public GameObject Life;
+    public bool AutoAdd;
+
+    public void Break(int Side)
+    {
+        if(Side <= 2)
+            SpawnLife();
+        else if(Side == 7)
+            SpawnLife();
+        else if(Side == 8)
+        {
+            AutoAdd = true;
+            SpawnLife();
+        }
+        else if(Side >= 9)
+        {
+            DisableCrate();   
+        }
+    }
+
+    public void DisableCrate()
+    {
+        gameObject.SetActive(false);
+    }
+
+    private void SpawnLife()
+    {
+        if (AutoAdd)
+        {
+            Debug.Log("auto add");
+        }
+        else
+        {
+            Instantiate(Life, transform.position, Quaternion.identity);
+        }
+        DisableCrate();
+    }
+}
