b30b6ccb7a1c3fc8aaa15ee2e8ea8655cb712e69
zombiehunternr1 <44666850+zombiehunternr1@users.noreply.github.com>
2021-04-20 12:41:27 +0200

    Implemented life functionality
    - Life now can interact with either an explosion or player action
    - Interaction with either an explosion or spin attack destroys the life
    - Interaction with the player without performing a spin attack results in collecting the life

---
+++ Classic Crash/Assets/Scripts/Collectables/Life.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Life : MonoBehaviour, IInteractable
+{
+    private BoxCollider HitBox;
+
+    private void Awake()
+    {
+        HitBox = GetComponent<BoxCollider>();
+    }
+
+    public void Interacting(int Side)
+    {
+        if (Side >= 1 && Side <= 6 || Side >= 8 && Side <= 9)
+        {
+            CollectItem();
+        }
+        else if (Side == 7 || Side >= 10)
+        {
+            DestroyItem();
+        }
+    }
+
+    private void CollectItem()
+    {
+        gameObject.SetActive(false);
+    }
+
+    private void DestroyItem()
+    {
+        Destroy(gameObject);
+    }
+}
