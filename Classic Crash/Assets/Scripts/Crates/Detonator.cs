4ba0b6e7c919f71ea130bfd04a0a6337883b634c
zombiehunternr1 <44666850+zombiehunternr1@users.noreply.github.com>
2021-04-20 14:22:38 +0200

    Detonator crate implemented
    Added in a detonator script that, once activated by the player, gets all the available nitro crates and explode them if they haven't already.

---
+++ Classic Crash/Assets/Scripts/Crates/Detonator.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Detonator : MonoBehaviour, IInteractable
+{
+    public void Interacting(int Side)
+    {
+        if(Side <= 2)
+        {
+            Detonate();
+        }
+        else if(Side == 7)
+        {
+            Detonate();
+        }
+    }
+
+    private void Detonate()
+    {
+        Nitro[] Crates = FindObjectsOfType(typeof(Nitro)) as Nitro[];
+
+        foreach(Nitro crate in Crates)
+        {
+            if (crate.enabled)
+            {
+                crate.Explode();
+            }
+        }
+        gameObject.GetComponent<Renderer>().enabled = false;
+    }
+}
