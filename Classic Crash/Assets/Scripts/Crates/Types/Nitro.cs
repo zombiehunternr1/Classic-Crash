ea62bf92f0086f959b3f0982984d1629cd83ad01
zombiehunternr1 <44666850+zombiehunternr1@users.noreply.github.com>
2021-04-26 12:57:14 +0200

    Nitro crate random bouncing animations
    - Added a small and big bounce animation to the nitro crate.
    - Created a coroutine that keeps looping if the nitro crate is allowed to bounce
    - Added in a random interval in the coroutine to make the moment the crate bounces up and down different each time.
    - Built in an extra check to see if the small bounce has played an x amount of time so the big bounce will be played instead and the counter gets reset

--- Classic Crash/Assets/Scripts/Crates/Types/Nitro.cs
+++ Classic Crash/Assets/Scripts/Crates/Types/Nitro.cs
@@ -5,6 +5,19 @@ using UnityEngine;
 public class Nitro : MonoBehaviour, ICrateBase
 {
     public ParticleSystem Explosion;
+    public bool CanBounce;
+
+    private Animator AnimNitro;
+    private int SmallhopsTriggered = 0;
+
+    private void Awake()
+    {
+        AnimNitro = GetComponent<Animator>();
+        if (CanBounce)
+        {
+            StartCoroutine(RandomBounce());
+        }
+    }
 
     public void Break(int Side)
     {
@@ -13,6 +26,7 @@ public class Nitro : MonoBehaviour, ICrateBase
 
     public void Explode()
     {
+        StopCoroutine(RandomBounce());
         DisableCrate();
         Instantiate(Explosion, transform.position, Quaternion.identity);
     }
@@ -20,5 +34,27 @@ public class Nitro : MonoBehaviour, ICrateBase
     public void DisableCrate()
     {
         gameObject.SetActive(false);
+    }
+
+    private IEnumerator RandomBounce()
+    {
+        float RandomCheck = Random.Range(0, 5);
+        while (RandomCheck > 0)
+        {
+            RandomCheck -= Time.deltaTime;
+            yield return RandomCheck;
+        }
+        float SelectRandom = Random.Range(0, 1);
+        if (SelectRandom < 0.5f && SmallhopsTriggered < 5)
+        {
+            SmallhopsTriggered++;
+            AnimNitro.SetTrigger("SmallHop");
+        }
+        else
+        {
+            SmallhopsTriggered = 0;
+            AnimNitro.SetTrigger("BigHop");
+        }
+        StartCoroutine(RandomBounce());
     }
 }
