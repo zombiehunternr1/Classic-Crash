b4b00b1f5ce412cb90d8bfad5bc35ba103034aca
zombiehunternr1 <44666850+zombiehunternr1@users.noreply.github.com>
2021-05-10 23:29:59 +0200

    Improved TotalCrates script
    Made the text variable BoxCrateUI hidden in the inspector since we only need to be able to access this variable from another script but doesn't have to be shown in the inspector.

--- Classic Crash/Assets/Scripts/Crates/Types/TotalCrates.cs
+++ Classic Crash/Assets/Scripts/Crates/Types/TotalCrates.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class TotalCrates : MonoBehaviour
 {
     public GameEventTransform CheckTotal;
+    [HideInInspector]
     public Text BoxCrateUI;
 
     private void Start()
