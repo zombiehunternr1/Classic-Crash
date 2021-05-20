a46ba4eaf0d125903896561cec6e4ca98139d805
zombiehunternr1 <44666850+zombiehunternr1@users.noreply.github.com>
2021-05-20 15:47:23 +0200

    Camera rotation added
    - Build in a check if the player is within the viewport of the camera we return the function. When he's out of the viewport area we rotate the camera's position slowly towards the player overtime.
    
    NOTE:
    Need to fix issue where if the player walks forward again the camera should adjust itself again so it's behind the player again

--- Classic Crash/Assets/Scripts/Camera/FollowTargetCamera.cs
+++ Classic Crash/Assets/Scripts/Camera/FollowTargetCamera.cs
@@ -6,7 +6,11 @@ public class FollowTargetCamera : MonoBehaviour
 {
     public Transform Target;
     public float SmoothSpeed = 0.125f;
+    public float SmoothRotation = 1f;
     public int Offset;
+
+    private Camera Camera;
+    private Vector3 ViewPos;
 
     private Vector3 StartPosition;
     private Vector3 DesiredPosition;
@@ -15,6 +19,7 @@ public class FollowTargetCamera : MonoBehaviour
     private void OnEnable()
     {
         StartPosition = transform.position;
+        Camera = GetComponent<Camera>();
     }
 
     private void FixedUpdate()
@@ -22,9 +27,20 @@ public class FollowTargetCamera : MonoBehaviour
         DesiredPosition = new Vector3(transform.position.x, transform.position.y, Target.position.z + Offset);
         SmoothPosition = Vector3.Lerp(transform.position, DesiredPosition, SmoothSpeed);
         transform.position = SmoothPosition;
+
         if (transform.position.z <= StartPosition.z)
         {
             transform.position = new Vector3(transform.position.x, transform.position.y, StartPosition.z);
+        }
+
+        ViewPos = Camera.WorldToViewportPoint(Target.position);
+        if (ViewPos.x >= 0.2 && ViewPos.x <= 0.8 && ViewPos.y >= 0 && ViewPos.y <= 1 && ViewPos.z > 0)
+        {
+            return;
+        }
+        else
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Target.position - transform.position), SmoothRotation * Time.fixedDeltaTime);
         }
     }
 }
